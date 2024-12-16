Major reworking of the file reading needed: ORCA does not always calculate all convergence criteria, this is currently not considered and leads to qualitatively wrong results. Current state broken

orcatools/go.py
@@ -4,14 +4,17 @@ import glob
 import numpy as np
 from matplotlib.ticker import MaxNLocator
 from matplotlib import pyplot as plt
+import ase.units as units
 
-def make_go_plot(xAxis, iterations, labels, filename, lw=3, s=0, show=False):
+def make_go_plot(xAxis, iterations, labels, filename, filelabels=None, lw=3, s=0, show=False):
     msbig = 9
+    if filelabels is None:
+        filelabels = labels
     for i_label, label in enumerate(labels):
         data = np.array([ d[i_label][0] for d in iterations ])
         data_converged = np.array([ d[i_label][-1] for d in iterations ])
-        converged_indices = np.where(data_converged is True)[0]
-        not_converged_indices = np.where(data_converged is False)[0]
+        converged_indices = np.where(data_converged)[0]
+        not_converged_indices = np.where(data_converged == False)[0]
         ax = plt.figure().gca()
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         plt.xlabel('Step Number')
@@ -22,7 +25,7 @@ def make_go_plot(xAxis, iterations, labels, filename, lw=3, s=0, show=False):
         plt.scatter(xAxis[converged_indices], data[converged_indices], marker='x', color='green', s=(msbig+s)**2)
         #plt.legend()
         plt.tight_layout()
-        plt.savefig("{}_{}.png".format(filename,label.replace(' ', '_')))
+        plt.savefig("{}_{}.png".format(filename,filelabels[i_label].replace(' ', '_')))
         if show:
             plt.show()
         plt.close()
@@ -40,7 +43,12 @@ def read_output(output_file):
                 n_iterations += 1
                 line = f.readline() #skip header
                 line = f.readline() #skip --- line
-                line = f.readline() #read first data line
+                if n_iterations == 1:
+                    # First step of geometry optimization does not have an energy change,
+                    # this leads to the plot being missing
+                    line = "Energy change      0.0000000000            0.0000050000      NO"
+                else:
+                    line = f.readline() #read first data line
                 while line:
                     tmp = line.split()
                     if n_iterations == 1:
@@ -55,7 +63,38 @@ def read_output(output_file):
             line = f.readline()
     return iterations, labels
 
-def plot_orca_go(filename='convergence.png', presentation=False, path='.', show=False):
+def convert_units(data, labels, target="au"):
+    unit_labels = labels.copy()
+    # print(data)
+    if target == "au":
+        for i in range(len(unit_labels)):
+            if unit_labels[i] == "Energy change":
+                unit_labels[i] = "Energy change / $E_h$"
+            if "gradient" in unit_labels[i]:
+                unit_labels[i] = unit_labels[i] + " / $E_h\\, a_0^{-1}$"
+            if "step" in unit_labels[i]:
+                unit_labels[i] = unit_labels[i] + " / $a_0$"
+    if target == "si":
+        for i in range(len(unit_labels)):
+            if unit_labels[i] == "Energy change":
+                unit_labels[i] = "Energy change / $\\mathrm{kJ}\\, \\mathrm{mol}^{-1}$"
+                # data = convert_data(data, i, units.Hartree * units.mol / units.kJ)
+            if "gradient" in unit_labels[i]:
+                unit_labels[i] = unit_labels[i] + " / $\\mathrm{kJ}\\, \\mathrm{mol}^{-1}\\, \\mathrm{nm}^{-1}$"
+                # data = convert_data(data, i, (units.Hartree * units.mol / units.kJ) * (units.nm / units.Bohr))
+            if "step" in unit_labels[i]:
+                unit_labels[i] = unit_labels[i] + " / $\\mathrm{nm}$"
+                # data = convert_data(data, i, units.Bohr / units.nm)
+    return data, unit_labels
+
+def convert_data(data, index, factor):
+    for i in range(len(data)):
+        pass
+        data[i][index][0] = data[i][index][0] * factor
+    return data
+
+
+def plot_orca_go(filename='convergence.png', presentation=False, path='.', show=False, unit="si"):
     # check for numerical subfolders
     subfolders = [ int(f) for f in glob.glob(os.path.join(path,'*')) if os.path.isdir(f) and f.isdigit() ]
     subfolders.sort()
@@ -80,6 +119,14 @@ def plot_orca_go(filename='convergence.png', presentation=False, path='.', show=
         assert os.path.isfile(output_file), "Output file {:} does not exist.".format(output_file)
         #print("Reading output file {:}".format(output_file))
         tmp, tmp_labels = read_output(output_file)
+        # Use add the documented units to the labels
+        tmp, unit_labels = convert_units(tmp, tmp_labels, target=unit)
+        for i, list in enumerate(tmp):
+            try:
+                test= list[4]
+            except:
+                print("I", i)
+                print("l", list)
         iterations.extend(tmp)
         labels.append(tmp_labels)
     print("Done!")
@@ -98,12 +145,13 @@ def plot_orca_go(filename='convergence.png', presentation=False, path='.', show=
         s = 0
 
     fn = os.path.join(path, filename)
-    make_go_plot(np.arange(1, n_iterations + 1), iterations, labels, filename=fn, lw=lw, s=s, show=show)
+    make_go_plot(np.arange(1, n_iterations + 1), iterations, unit_labels, filelabels=labels, filename=fn, lw=lw, s=s, show=show)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot ORCA Geometry Optimization Convergence')
     parser.add_argument('--file', help='Plot Filename Beginning, will be appended with _<label>.png', default='convergence')
     parser.add_argument('--presentation', help='Presentation Mode (i.e. thicker lines)', action='store_true')
+    parser.add_argument("--unit", "-u", help="Unit System to use for plotting, choose between 'au' or 'si'", default='si')
     args = parser.parse_args()
-    plot_orca_go(args.file, args.presentation)
+    plot_orca_go(args.file, presentation=args.presentation, unit=args.unit)